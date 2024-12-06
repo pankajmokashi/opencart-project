c791a9623c85f0fc652c9c9ff09b420e45a1e80f
pankajmokashi <pankajmokashi12@gmail.com>
2024-12-06 21:11:32 +0530

    feat(Breadcrumbs): uapdate Breadcrumbs component

--- src/components/Breadcrumbs.jsx
+++ src/components/Breadcrumbs.jsx
@@ -6,25 +6,28 @@ function Breadcrumbs() {
   const pathnames = location.pathname.split("/").filter((x) => x);
 
   return (
-    <ol className="px-4 sm:px-6 text-sm flex items-center border bg-light-gray text-nav-blue breadcrumb">
-      <li className="pr-10 sm:pr-12 h-10 flex items-center relative breadcrumb-item">
+    <ol className="px-4 sm:px-6 text-xs sm:text-sm flex items-center border bg-light-gray text-nav-blue breadcrumb">
+      <li className="h-10 flex items-center">
         <Link to="/">
           <span className="w-5 flex">
             <Home />
           </span>
         </Link>
+        <span className="mx-2 sm:mx-4 text-lg sm:text-xl text-slate-600">
+          /
+        </span>
       </li>
       {pathnames.map((value, index) => {
         const to = `/${pathnames.slice(0, index + 1).join("/")}`;
 
         return (
-          <li
-            key={to}
-            className="pr-10 sm:pr-12 h-10 flex items-center relative breadcrumb-item"
-          >
+          <li key={to} className="h-10 flex items-center">
             <Link to={to}>
               {value.charAt(0).toUpperCase() + value.slice(1)}
             </Link>
+            <span className="mx-2 sm:mx-4 text-lg sm:text-xl text-slate-600">
+              /
+            </span>
           </li>
         );
       })}
